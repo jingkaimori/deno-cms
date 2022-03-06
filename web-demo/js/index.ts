0845277c0aa9286ada4cf5cbdbba568b8d0d3cc9
jingkaimori <jingkaimori@gmail.com>
2022-03-06 10:34:32 +0800

    add format selector

--- web-demo/js/index.ts
+++ web-demo/js/index.ts
@@ -2,36 +2,59 @@ import * as xwiki from "../../parsers/xwiki.ts";
 import type { treeNode, rootNode, generalNode } from "../../macros/macros.ts";
 import * as markdown from "../../parsers/borrowed/markdown.ts"
 import * as tmml from "../../parsers/borrowed/texmacs-tmml.ts"
-import { Site, ArticleLocal } from "../../types/repository.ts";
+import { Site } from "../../types/repository.ts";
 import { getArticleTitle, mapNode } from "./render.ts";
 import * as path from "https://deno.land/std@0.90.0/path/mod.ts"
 
 const mode = {meta:"local",format:"tmml"};
 const treeHTMLMap:WeakMap<HTMLElement,treeNode<generalNode>> = new WeakMap();
-if(mode.meta == "xwiki"){
-  const res = await fetch("./export/package.xml");
-  const parser = new DOMParser();
-  const tree = parser.parseFromString(await res.text(), "text/xml") as XMLDocument;
-  const files = tree.querySelectorAll("package > files > *");
-  files.forEach((v) => {
-    let li = document.createElement("li");
-    li.classList.add("filename");
-    li.innerText = String(v.firstChild?.nodeValue);
-    li.addEventListener("click", handleClick);
-    (document.querySelector("#list") as HTMLUListElement).append(li);
-  });
-}else if(mode.meta == "local"){
-  const res = await fetch("./export/meta.json");
-  const tree = JSON.parse(await res.text()) as Site;
-  const files = tree.articles;
-  files.forEach((v) => {
-    let li = document.createElement("li");
-    li.classList.add("filename");
-    li.innerText = String(v.path);
-    li.addEventListener("click", handleClick);
-    (document.querySelector("#list") as HTMLUListElement).append(li);
-  });
 
+const selectElem = document.querySelector("#format") as HTMLSelectElement
+selectElem.addEventListener("change",(_ev)=>{
+  console.log("emit")
+  if(selectElem.value == "local-tmml"){
+    mode.meta="local"
+    mode.format="tmml"
+  }else if(selectElem.value == "local-markdown"){
+    mode.meta="local"
+    mode.format="markdown"
+  }else if(selectElem.value == "xwiki-xwiki"){
+    mode.meta="xwiki"
+    mode.format="xwiki"
+  }else{
+    console.error("unsupported select")
+  }
+  onFormatChange()
+})
+
+const onFormatChange = async () => {
+  const list = document.querySelector("#list") as HTMLUListElement
+  clearChilds(list)
+  if(mode.meta == "xwiki") {
+    const res = await fetch("./export/package.xml");
+    const parser = new DOMParser();
+    const tree = parser.parseFromString(await res.text(), "text/xml") as XMLDocument;
+    const files = tree.querySelectorAll("package > files > *");
+    files.forEach((v) => {
+      const li = document.createElement("li");
+      li.classList.add("filename");
+      li.innerText = String(v.firstChild?.nodeValue);
+      li.addEventListener("click", handleClick);
+      list.append(li);
+    });
+  } else if(mode.meta == "local") {
+    const res = await fetch("./export/meta.json");
+    const tree = JSON.parse(await res.text()) as Site;
+    const files = tree.articles;
+    files.forEach((v) => {
+      const li = document.createElement("li");
+      li.classList.add("filename");
+      li.innerText = String(v.path);
+      li.addEventListener("click", handleClick);
+      list.append(li);
+    });
+
+  }
 }
 
 async function handleClick(this:HTMLLIElement) {
@@ -45,23 +68,21 @@ async function handleClick(this:HTMLLIElement) {
         "<?xml version='1.1",
         "<?xml version='1.0",
       );
-      let xmltree = parser.parseFromString(text, "text/xml") as XMLDocument;
+      const xmltree = parser.parseFromString(text, "text/xml") as XMLDocument;
       console.log(xmltree);
-      let content = String(
+      const content = String(
         xmltree.querySelector("xwikidoc > content")?.firstChild?.nodeValue,
       );
-      let {success:res,leftstr:rest,tree} = xwiki.doc(content);
+      const {success:res,leftstr:rest,tree} = xwiki.doc(content);
       xwiki.postprocess(tree);
       renderResult(content,res,rest);
       renderDoc(tree);
     }else if(mode.format == "md"){
-      let url ;
       const first = await fetch(path.join("./export",filename));
       const text = await first.text();
       const {success,leftstr,tree} = markdown.doc(text);
       renderResult(text,success,leftstr);
       renderDoc(tree);
-      //renderDoc(text)
     }else if(mode.format == "tmml"){
       const first = await fetch(path.join("./export",filename));
       const text = await first.text();
@@ -78,9 +99,9 @@ async function handleClick(this:HTMLLIElement) {
  * @param res 
  * @param rest the part left by parser
  */
-function renderResult(content:string,res:boolean,rest:string) {
+const renderResult = (content:string,res:boolean,rest:string) => {
 
-  let statElem=
+  const statElem=
     document.querySelector("#stat") as HTMLParagraphElement;
   if(res){
     statElem.classList.add("success");
@@ -100,7 +121,8 @@ function renderResult(content:string,res:boolean,rest:string) {
     let begini = splitstr.lastIndexOf("\n")
     let beginp = splitstr;
     if(begini>0){
-    beginp = splitstr.slice(begini);}
+      beginp = splitstr.slice(begini);
+    }
 
     let endi = rest.indexOf('\n')
     let endp = rest;
@@ -113,7 +135,7 @@ function renderResult(content:string,res:boolean,rest:string) {
   }
 }
 
-function onDocEdit(ev:InputEvent) {
+function _onDocEdit(ev:InputEvent) {
   const range = ev.getTargetRanges()[0]
   range.startContainer.nodeValue;
   console.group()
@@ -160,7 +182,7 @@ const renderDoc = (tree:Readonly<treeNode<rootNode>>) => {
   
 }
 
-function clearChilds(element: HTMLElement): void {
+const clearChilds = (element: HTMLElement): void => {
   Array.from(element.childNodes)
     .forEach(element.removeChild, element);
 }
