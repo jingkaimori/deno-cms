968d7ac07f14dfd0a05da8ecfbb5f4d02c9492ea
jingkaimori <jingkaimori@gmail.com>
2022-11-27 11:11:14 +0800

    simplify imports

--- deps.ts
+++ deps.ts
@@ -1,6 +1,5 @@
 export { assertThrows,assertObjectMatch,assertEquals } from "https://deno.land/std@0.155.0/testing/asserts.ts";
 export * as path from "https://deno.land/std@0.166.0/path/mod.ts"
 
-// @deno-types="https://cdn.jsdelivr.net/npm/@types/marked@4.0.7/index.d.ts"
-export * from "https://cdn.jsdelivr.net/npm/marked@4.2.3/lib/marked.esm.js";
+export * from "https://deno.land/x/marked@1.0.1/mod.ts";
 
