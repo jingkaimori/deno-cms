b9a102e4f3b8a6dec6650431b841522922f1dd54
jingkaimori <jingkaimori@gmail.com>
2022-12-01 13:13:01 +0800

    types for ongoing server setup

---
+++ types/errors.ts
@@ -0,0 +1,5 @@
+export enum status {
+    SUCCESS = 0,
+    INNER_ERROR = 1,
+    RESOURCE_NOT_FOUND = 1,
+}
--- types/repository.ts
+++ types/repository.ts
@@ -1,6 +1,22 @@
 export type Site = {
-    articles:Article[],
+    articles: Article[],
 }
+
+export type Remote<T> = {
+    [key in keyof T]: Remote<T[key]>
+}
+
+export type RemoteValue =
+  | { [key: string]: RemoteValue | RemoteHash | Uint8Array }
+  | RemoteValue[]
+  | string
+  | number
+  | boolean
+  | null;
+
+export type RemoteHash<T extends RemoteValue = null> = Uint8Array;
+
+export type RemoteContent<T extends RemoteValue = null> = T | RemoteHash<T>
 
 export type Article = ArticleLocal | ArticleRemote
 export type ArticleLocal = {
