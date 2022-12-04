6275fcdcb1eb246ad48c0427dd2162a27169ac58
jingkaimori <jingkaimori@gmail.com>
2022-12-04 11:37:18 +0800

    server rpc handler

--- server/protocol.ts
+++ server/protocol.ts
@@ -1,39 +1,191 @@
-import { plainDocument } from "../types/data.ts";
-import { retrieve, update } from "./types.ts"
-import { RemoteContent, RemoteHash, RemoteValue } from "../types/repository.ts";
+import { RemoteContent, RemoteValue } from "../types/repository.ts";
+import { status } from "../types/errors.ts";
+
+type serverReturnType = void | RemoteContent<RemoteValue> | status
 
 type serverFunctions = {
-    [_: string]: (...t: (RemoteValue|RemoteHash)[]) => void
+    [_ in string]: 
+        // deno-lint-ignore no-explicit-any
+        (this:void, ...t: any[]) 
+            => serverReturnType
 }
 
-let testarticle:plainDocument = {name:"",childs:[],raw:"",auxilary:{}}
 
-type webinterface = {
-    readArticle:retrieve<plainDocument>
-    updateArticle:update<plainDocument>
+type Channel  = AsyncIterable<string> & {
+    send(message:string): void
 }
 
-const server:webinterface = {
-    readArticle:()=>{
-        return testarticle
-    },
-    updateArticle:()=>{
-        return;
-    }
+type Request<T extends serverFunctions = serverFunctions, method extends keyof T= keyof T> = {
+    name: method,
+    param: Parameters<T[method]>,
+    id?: number
+}
+type Response<Return extends serverReturnType = serverReturnType> = {
+    returns: Return
+    id: number
 }
 
-type lowconnect = {
-    receive: ReadableStream<string>
-    send: (msg:string)=>void
-}
-
-class Server<T extends RemoteContent> {
+export class Server<T extends serverFunctions> {
     #handler:T;
+    #channel: Channel| null;
     constructor(handler:T) {
         this.#handler = handler
+        this.#channel = null
     }
 
-    listen(){
+    async listen(channel:Channel) {
+        if (this.#channel) {
+            this.close()
+        }
+        this.#channel = channel
+        for await (const message of channel) {
+            const req = JSON.parse(message) as Request<T>
+            
+            const func = this.#handler[req.name]
+            const res = func(...req.param)
+            
+            if (req.id !== undefined) {
+                const resp:Response = {
+                    returns:res,
+                    id: req.id
+                }
+
+                channel.send(JSON.stringify(resp))
+            }
+        }
+    }
+
+    close(){
+        if (this.#channel === null) {
+            return
+        }
+        const res = this.#channel[Symbol.asyncIterator]()
+        if(res.return !== undefined){
+            res.return()
+        }
+    }
+}
+
+export class Client<T extends serverFunctions> {
+    #channel?: Channel
+    #cachedResponse : Map<number,serverReturnType>
+    #ids: number
+    constructor() {
+        this.#cachedResponse = new Map()
+        this.#ids = 0
+    }
+
+    connect(channel:Channel){
+        if (this.#channel) {
+            this.close()
+        }
+        this.#channel = channel
+    }
+    async call<name extends keyof T = keyof T>(
+        method: name, ...params:Parameters<T[name]>
+    ): Promise<ReturnType<T[name]>>{
+        if (this.#channel === undefined) {
+            throw new Error("connection is not established, please connect first");
+        }
+        
+        const reqid = this.#ids
+        const req:Request<T> ={
+            name: method,
+            param: params,
+            id: reqid
+        }
+        this.#channel?.send(JSON.stringify(req))
+
+        do {
+            if (this.#cachedResponse.has(reqid)) {
+                return this.#cachedResponse.get(reqid) as ReturnType<T[name]>
+            }
+            const iterres = await this.#channel[Symbol.asyncIterator]().next()
+            if (iterres.done) {
+                throw new Error("response not appear")
+            }else{
+                const resp = JSON.parse(iterres.value) as Response<ReturnType<T[name]>>
+                if (resp.id == reqid) {
+                    return resp.returns
+                } else {
+                    this.#cachedResponse.set(resp.id,resp.returns)
+                }
+            }
+        } while(true)
 
     }
+    close(){
+        if (this.#channel === undefined) {
+            return
+        }
+        const res = this.#channel[Symbol.asyncIterator]()
+        if(res.return !== undefined){
+            res.return()
+        }
+    }
+}
+
+export function webSocket2Channel(socket:WebSocket):Promise<Channel> {
+    return new Promise((resolve,reject) => {
+        socket.onopen = ()=>{
+            resolve(socketWrapper(socket))
+            socket.onopen = null
+        }
+        socket.onerror = ()=>{
+            reject()
+        }
+    })
+}
+
+function socketWrapper(socket: WebSocket): Channel {
+  return {
+    send(string) {
+      socket.send(string);
+    },
+    [Symbol.asyncIterator]() {
+      return {
+        next(): Promise<IteratorResult<string, undefined>> {
+          return new Promise((resolve, reject) => {
+            socket.onmessage = async (ev) => {
+              let value = '';
+              if(typeof ev.data == 'string' || ev.data instanceof String) {
+                value = ev.data.valueOf();
+              } else if(ev.data instanceof Blob) {
+                value = await ev.data.text();
+              } else if(ev.data instanceof ArrayBuffer) {
+                value = await new Blob([ev.data]).text();
+              }
+              resolve({
+                done: false,
+                value
+              });
+            };
+            socket.onerror = (ev) => {
+              reject(ev);
+            };
+            socket.onclose = (_e) => {
+              resolve({
+                done: true,
+                value: undefined
+              });
+            };
+          });
+        },
+        return(): Promise<IteratorResult<string, undefined>> {
+          socket.close();
+          return Promise.resolve({
+            done: true,
+            value: undefined
+          });
+        },
+        throw(): Promise<IteratorResult<string, undefined>> {
+          socket.close();
+          return Promise.reject({
+            done: true,
+            value: undefined
+          });
+        }
+      };
+    }
+  };
 }
--- server/types.ts
+++ server/types.ts
@@ -1,7 +1,7 @@
 import { status } from "../types/errors.ts";
 import { RemoteHash, RemoteValue } from "../types/repository.ts";
 
-export type create<T extends RemoteValue> = (content:T) => RemoteHash<T> | status.INNER_ERROR;
-export type retrieve<T extends RemoteValue> = (id:RemoteHash<T>) => T | status.INNER_ERROR | status.RESOURCE_NOT_FOUND
-export type update<T extends RemoteValue> = (content:T, id:RemoteHash<T>) => void | status.RESOURCE_NOT_FOUND;
-export type deletedata<T extends RemoteValue> = (id:RemoteHash<T>) => void | status.INNER_ERROR | status.RESOURCE_NOT_FOUND
+export type create<T extends RemoteValue> = (this:void,content:T) => RemoteHash<T> | status.INNER_ERROR;
+export type retrieve<T extends RemoteValue> = (this:void,id:RemoteHash<T>) => T | status.INNER_ERROR | status.RESOURCE_NOT_FOUND
+export type update<T extends RemoteValue> = (this:void,content:T, id:RemoteHash<T>) => void | status.RESOURCE_NOT_FOUND;
+export type deletedata<T extends RemoteValue> = (this:void,id:RemoteHash<T>) => void | status.INNER_ERROR | status.RESOURCE_NOT_FOUND
